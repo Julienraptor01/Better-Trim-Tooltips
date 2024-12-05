408907d31facfda6658d3ab41bb402bc7a1b2bd4
Julienraptor01 <julienraptor01@gmail.com>
2024-12-05 04:08:58 +0100

    update to 1.20.6

--- src/client/java/io/github/andrew6rant/bettertrimtooltips/mixin/client/ArmorTrimMixin.java
+++ src/client/java/io/github/andrew6rant/bettertrimtooltips/mixin/client/ArmorTrimMixin.java
@@ -1,76 +1,51 @@
 package io.github.andrew6rant.bettertrimtooltips.mixin.client;
 
-import com.mojang.serialization.DataResult;
-import net.minecraft.item.ItemStack;
+import net.minecraft.client.item.TooltipType;
+import net.minecraft.item.Item;
 import net.minecraft.item.trim.ArmorTrim;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.nbt.NbtOps;
-import net.minecraft.registry.DynamicRegistryManager;
-import net.minecraft.registry.RegistryOps;
+import net.minecraft.item.trim.ArmorTrimMaterial;
+import net.minecraft.item.trim.ArmorTrimPattern;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextContent;
-import net.minecraft.text.TranslatableTextContent;
 import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.List;
-import java.util.Optional;
-
-import static io.github.andrew6rant.bettertrimtooltips.BetterTrimTooltips.isStackedTrimsEnabled;
-import static net.minecraft.item.trim.ArmorTrim.getTrim;
+import java.util.function.Consumer;
 
 @Mixin(ArmorTrim.class)
-public class ArmorTrimMixin {
+public abstract class ArmorTrimMixin {
+	@Shadow
+	@Final
+	private boolean showInTooltip;
 
-    @Inject(at = @At("TAIL"), method = "appendTooltip(Lnet/minecraft/item/ItemStack;Lnet/minecraft/registry/DynamicRegistryManager;Ljava/util/List;)V")
-    private static void appendTooltip(ItemStack stack, DynamicRegistryManager registryManager, List<Text> tooltip, CallbackInfo info) {
-        Optional<ArmorTrim> optional = getTrim(registryManager, stack);
-        if (optional.isPresent()) {
-            // remove all the existing trim tooltips
-            // using removeIf to avoid ConcurrentModificationException
-            tooltip.removeIf(text -> {
-                TextContent textContent = text.getContent();
-                if (textContent instanceof TranslatableTextContent) {
-                    return ((TranslatableTextContent) textContent).getKey().equals("item.minecraft.smithing_template.upgrade");
-                }
-                if(text.getSiblings().size() == 1) {
-                    TextContent siblingContext = text.getSiblings().get(0).getContent();
-                    if (siblingContext instanceof TranslatableTextContent) {
-                        return ((TranslatableTextContent) siblingContext).getKey().contains("trim");
-                    }
-                }
-                return false;
-            });
+	@Shadow
+	public abstract RegistryEntry<ArmorTrimPattern> getPattern();
 
-            if (isStackedTrimsEnabled) {       // If StackedTrims mod is installed, iterate through the list of trims
-                assert stack.getNbt() != null; // this code is modified from StackedTrims' ArmorTrimItemMixin
-                NbtList nbtList = stack.getNbt().getList("Trim", 10);
-                for (NbtElement nbtElement : nbtList) {
-                    DataResult<ArmorTrim> result = ArmorTrim.CODEC.parse(RegistryOps.of(NbtOps.INSTANCE, registryManager), nbtElement);
-                    ArmorTrim armorTrim = result.result().orElse(null);
-                    if (armorTrim == null) continue;
-                    appendTrimTooltip(tooltip, armorTrim);
-                }
-            } else {
-                ArmorTrim armorTrim = optional.get();
-                appendTrimTooltip(tooltip, armorTrim);
-            }
-        }
-    }
+	@Shadow
+	public abstract RegistryEntry<ArmorTrimMaterial> getMaterial();
 
-    private static void appendTrimTooltip(List<Text> tooltip, ArmorTrim armorTrim) {
-        Style materialStyle = armorTrim.getMaterial().value().description().getStyle();
-        tooltip.add(Text.literal("") // I am unable to start with the description, so I have to add empty text at the start to append to
-                .append(armorTrim.getPattern().value().description()).formatted(Formatting.GRAY)
-                .append(ScreenTexts.space()
-                .append(Text.literal("(").setStyle(materialStyle)) // I made the "(" and ")" in code instead of in en_us.json to better support other languages
-                .append(armorTrim.getMaterial().value().description()))
-                .append(Text.literal(")").setStyle(materialStyle)));
-    }
+	/**
+	 * @author Julienraptor01
+	 * @reason change how the text is displayed in the tooltip until a more compatible method is found
+	 */
+	@Overwrite
+	public void appendTooltip(Item.TooltipContext context, Consumer<Text> tooltip, TooltipType type) {
+		if (showInTooltip) {
+			Style style = this.getMaterial().value().description().getStyle();
+			tooltip.accept(Text.literal("")
+					.append(this.getPattern().value().description())
+					.formatted(Formatting.GRAY)
+					.append(ScreenTexts.space())
+					.append(Text.literal("(")
+							.setStyle(style)
+							.append(this.getMaterial().value().description())
+							.append(")")
+							.setStyle(style)));
+		}
+	}
 }
